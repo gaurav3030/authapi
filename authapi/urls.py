784088165fcfa260245a18da24d296573f84925e
Vedant Sawant <vedant.sawant.2604@gmail.com>
2021-05-19 10:45:46 +0530

    URL changes

--- authapi/urls.py
+++ authapi/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('regface/', views.index, name='index'),
-    path('logface/', views.facelogin, name='facelogin'),
-    path('regvoice/', views.voiceregister, name='voiceregister'),
-    path('logvoice/', views.voicelogin, name='voicelogin'),
+    path('regface', views.index, name='index'),
+    path('logface', views.facelogin, name='facelogin'),
+    path('regvoice', views.voiceregister, name='voiceregister'),
+    path('logvoice', views.voicelogin, name='voicelogin'),
     
 ]
